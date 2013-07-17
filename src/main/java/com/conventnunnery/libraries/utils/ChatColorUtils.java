eee8272989839b61193adfb9c71aa77e7e4c6cb6
Richard Harrah <topplethenunnery@gmail.com>
2013-07-17 12:11:58 -0400

    adding some ChatColor methods

--- src/main/java/com/conventnunnery/libraries/utils/ChatColorUtils.java
+++ src/main/java/com/conventnunnery/libraries/utils/ChatColorUtils.java
@@ -1,6 +1,9 @@
 package com.conventnunnery.libraries.utils;
 
+import com.google.common.collect.Sets;
 import org.bukkit.ChatColor;
+
+import java.util.Set;
 
 public final class ChatColorUtils {
 
@@ -19,5 +22,24 @@ public final class ChatColorUtils {
             chatColor = fallback;
         }
         return chatColor;
+    }
+
+    public static ChatColor getRandomChatColorWithoutColors(ChatColor... colors) {
+        Set<ChatColor> colors1 = Sets.newHashSet(ChatColor.values());
+        for (ChatColor c : colors) {
+            if (colors1.contains(c)) {
+                colors1.remove(c);
+            }
+        }
+        return getRandomChatColorFromSet(colors1);
+    }
+
+    public static ChatColor getRandomChatColor() {
+        return getRandomChatColorFromSet(Sets.newHashSet(ChatColor.values()));
+    }
+
+    public static ChatColor getRandomChatColorFromSet(Set<ChatColor> chatColors) {
+        ChatColor[] chatColors1 = chatColors.toArray(new ChatColor[chatColors.size()]);
+        return chatColors1[(int) RandomUtils.randomRangeWholeExclusive(0, chatColors1.length)];
     }
 }
